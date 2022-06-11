e424530fee5cb027d25606f7e64c629626fb6dc0
Nazariy Dumanskyy <nazariy1995@gmail.com>
2022-06-11 15:36:28 -0700

    ADD: Day 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import backgroundVideo from "./assets/background.mp4";
+import nftVideo from "./assets/nftvideo.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <video className='background-video' src={backgroundVideo} width="600" height="300" autoPlay playsInline muted loop/>
+      <div className='main'>
+        <div className='top'>
+          <div className='image-container'>
+            <video className='nft-video' src={nftVideo} width="600" height="300" autoPlay playsInline muted loop/>
+          </div>
+          <div className='info'>
+              <h2>Adidos: INTO THE METAVERSE</h2>
+              <p>12 minted / 200</p>
+              <div className='actions'>
+                <button className='filled-button'>
+                  Connect Wallet
+                </button>
+              </div>
+          </div>
+        </div>
+        <div className='footer'>
+            MINTING NOW
+        </div>  
+      </div>
     </div>
   );
 }
